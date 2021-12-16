c7160817dc175652b7445061e57a768edb0fa3fe
cardpay-ci <rnd@cardpay.com>
2021-12-16 07:31:47 +0000

    Published latest changes

--- src/main/java/com/cardpay/sdk/client/ApiClient.java
+++ src/main/java/com/cardpay/sdk/client/ApiClient.java
@@ -51,7 +51,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class ApiClient {
-    public static final String USER_AGENT = "CardpaySdk/3.10.16/Java";
+    public static final String USER_AGENT = "CardpaySdk/3.10.17/Java";
     public static final Optional<ProxySelector> ENV_VAR_PROXY_SELECTOR = createEnvVarProxySelector();
 
     private TokenProvider tokenProvider;
